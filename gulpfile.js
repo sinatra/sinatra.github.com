e103c7f3c5c3567da2b56ac30137a4b6de6f11e5
ed wassermann <vis0vis@gmail.com>
2016-07-25 18:16:35 +0200

    Update gulpfile
    Add two tasks for css and js assets
    Compile and compress sass files
    Uglify js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 var gulp   = require('gulp'),
-    uglify = require('gulp-uglify');
+    uglify = require('gulp-uglify'),
+    sass   = require('gulp-ruby-sass'),
     rename = require('gulp-rename');
 
-gulp.task('default', function(){
+gulp.task('js', function(){
   gulp.src(['js/*.js', '!js/*.min.js'])
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'}))
-    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('js/min'));
+});
+
+gulp.task('css', function(){
+  sass('_sass/application.sass', { style: 'compressed' })
+    .pipe(rename({
+      suffix: '.min'}))
+    .pipe(gulp.dest('css'));
 });
