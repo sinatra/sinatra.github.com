d04ddf1eeb85f4f2de39a0ce384407158c5be4e7
ed wassermann <vis0vis@gmail.com>
2016-07-30 19:19:39 +0200

    Update gulpfile
    Add del and runSequence
    Use return in tasks
    Add build-css task
    Add clean-css task
    Little tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
-var gulp     = require('gulp'),
-    uglify   = require('gulp-uglify'),
-    sass     = require('gulp-ruby-sass'),
-    prefixer = require('gulp-autoprefixer'),
-    rename   = require('gulp-rename');
+var gulp        = require('gulp'),
+    uglify      = require('gulp-uglify'),
+    sass        = require('gulp-ruby-sass'),
+    prefixer    = require('gulp-autoprefixer'),
+    purify      = require('gulp-purifycss'),
+    rename      = require('gulp-rename'),
+    del         = require('del'),
+    runSequence = require('run-sequence');
+
+
+gulp.task('watch', function(){
+  gulp.watch('_sass/*.sass', ['css']),
+  gulp.watch('css/min/*.css', ['prefix']);
+});
+
+// JS stuff
 
 gulp.task('js', function(){
-  gulp.src(['js/*.js', '!js/*.min.js'])
+  return gulp.src(['js/*.js', '!js/*.min.js'])
     .pipe(uglify())
     .pipe(rename({
       suffix: '.min'}))
     .pipe(gulp.dest('js/min'));
 });
 
-gulp.task('css', function(){
-  sass('_sass/application.sass', { style: 'compressed' })
-    .pipe(rename({
-      suffix: '.min'}))
-    .pipe(gulp.dest('css/min'));
+// Sass/CSS stuff
+
+gulp.task('build-css', function(callback) {
+  runSequence('sass2css',
+              'prefix',
+              'purify',
+              'clean-css',
+              callback);
+});
+
+gulp.task('sass2css', function(){
+  console.log("Buidling your CSS from Sass");
+  return sass('_sass/application.sass')
+  //return sass('_sass/application.sass', { style: 'compressed' })
+    .pipe(gulp.dest('css/development/'));
 });
 
 gulp.task('prefix', function(){
-  gulp.src('css/min/application.min.css')
+  console.log("Starting autoprefixer");
+  return gulp.src('css/development/*.css')
     .pipe(prefixer({
       browsers: [
         '> 1%',
@@ -35,10 +57,16 @@ gulp.task('prefix', function(){
       ],
       cascade: false
         }))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css/prefixed'));
 });
 
-gulp.task('watch', function(){
-  gulp.watch('_sass/*.sass', ['css']),
-  gulp.watch('css/min/application.min.css', ['prefix']);
+gulp.task('purify', function() {
+  console.log("Purifying your CSS from unused styles and minifying it");
+  return gulp.src(['css/prefixed/*.css'])
+    .pipe(purify(['_site/*.html'], { minify: true, rejected: true }))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('clean-css', function() {
+  return del(['css/prefixed']);
 });
